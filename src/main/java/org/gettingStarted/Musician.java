6b6f454e4722682f639bb6e2e029a53f770b9e58
HidiAlgo <hashan.mahesh01@gmail.com>
2025-05-31 16:10:16 +0530

    creating first api call

---
+++ src/main/java/org/gettingStarted/Musician.java
@@ -0,0 +1,3 @@
+package org.gettingStarted;
+
+public record Musician( String name, String albums ) {}
---
+++ src/main/java/org/gettingStarted/MusicianAssistant.java
@@ -0,0 +1,58 @@
+package org.gettingStarted;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.output.Response;
+
+import java.time.Duration;
+import java.util.List;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+import static java.lang.System.exit;
+
+public class MusicianAssistant
+{
+    private static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
+
+    public static void main(String[] args)
+    {
+        String name = args[0];
+
+        ChatLanguageModel chatLanguageModel = OpenAiChatModel.builder()
+                .baseUrl("http://langchain4j.dev/demo/openai/v1") // using a free version of the openai just for learning purpose
+                .apiKey( OPENAI_API_KEY )
+                .modelName( GPT_4_O_MINI )
+                .temperature( 0.3 )
+                .timeout( Duration.ofSeconds(60) )
+                .logRequests( true )
+                .logRequests( true )
+                .build();
+
+        Musician musician = new MusicianAssistant().generateTopThreeAlbums(chatLanguageModel, name);
+        System.out.println(musician);
+        exit(0);
+    }
+
+    private Musician generateTopThreeAlbums( ChatLanguageModel model, String name )
+    {
+        SystemMessage systemMessage = SystemMessage.from("""
+                You are an expert in Jazz music.
+                Reply with only the names of the artists, albums, etc.
+                Be very concise.
+                If a list is given, separate the items with commas.
+                """
+        );
+
+        UserMessage message = UserMessage.from(
+                String.format("Only list the top 3 albums of %s", name)
+        );
+        List<ChatMessage> messages = List.of( systemMessage, message );
+        Response<AiMessage> aiMessageResponse = model.generate( messages );
+        String topThreeAlbums = aiMessageResponse.content().text();
+
+        return new Musician( topThreeAlbums, name );
+    }
+}
